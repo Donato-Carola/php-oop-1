c76cf3a8f615671ae54397dc816e9d2789d456a8
Donato-Carola <don94@live.it>
2024-01-25 18:17:33 +0100

    recent changes

--- index.php
+++ index.php
@@ -5,9 +5,11 @@ include_once __DIR__ . '/Models/Movie.php';
 
 
 
-$newMovie = new Movie('https://i.ebayimg.com/images/g/OBoAAOSw2vBfbDFb/s-l1600.jpg', 'Avengers', 'Action Movie', 'I leggendari supereroi Iron Man, Hulk, Thor, Capitan America, Occhio di Falco e la Vedova Nera vengono reclutati da un\' agenzia governativa segreta per combattere un nemico inatteso che minaccia la sicurezza della Terra.');
+$firstMovie = new Movie('https://i.ebayimg.com/images/g/OBoAAOSw2vBfbDFb/s-l1600.jpg', 'Avengers', 'Action Movie', 'I leggendari supereroi Iron Man, Hulk, Thor, Capitan America, Occhio di Falco e la Vedova Nera vengono reclutati da un\' agenzia governativa segreta per combattere un nemico inatteso che minaccia la sicurezza della Terra.');
+$secondMovie = new Movie('https://pad.mymovies.it/filmclub/2012/08/035/locandina.jpg', 'Avengers  - Age of ultron', 'Action Movie', 'Gli Avengers si ricongiungono allo scopo di unire le proprie forze a quelle dei nuovi arrivati Quicksilver e Scarlet. Il loro obiettivo è sconfiggere il robot malefico chiamato Ultron, creato da Tony Stark con intenzioni pacifiche e poi trasformato in una macchina assetata di sangue.');
 
-$listMovie = [$newMovie, $newMovie, $newMovie, $newMovie,];
+
+$listMovie = [$firstMovie, $secondMovie, $firstMovie, $secondMovie];
 
 ?>
 
@@ -23,7 +25,7 @@ $listMovie = [$newMovie, $newMovie, $newMovie, $newMovie,];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>movie</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="./css/style.css">
+    <link href="./css/style.css" rel="stylesheet">
 </head>
 
 <body>
@@ -34,15 +36,15 @@ $listMovie = [$newMovie, $newMovie, $newMovie, $newMovie,];
         <section class="row">
 
             <?php foreach ($listMovie as $movie) { ?>
-                <div class="col-3">
-                <div class="card" style="width: 18rem;">
-                    <img src="<?php echo $movie->img; ?>" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $movie->title; ?></h5>
-                        <h5 class="card-text"><?php echo $movie->getTrama(); ?></h5>
-                        <p><?php echo $movie->description; ?></p>
+                <div class="col-sm-12 col-md-6 col-lg-3 ">
+                    <div class="card mb-3" >
+                        <img src="<?php echo $movie->img; ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $movie->title; ?></h5>
+                            <h5 class="card-text"><?php echo $movie->getTrama(); ?></h5>
+                            <p><?php echo $movie->description; ?></p>
+                        </div>
                     </div>
-                </div>
 
                 </div>
 
